f73220bd8ee3e005504be6c1df78d3aac209f3ee
Richard Nash <rvnash@gmail.com>
2024-12-01 17:34:06 -0500

    Refactor

--- d01/test/d01_test.exs
+++ d01/test/d01_test.exs
@@ -3,7 +3,6 @@ defmodule D01Test do
   doctest D01
 
   def read_lists do
-    IO.puts("Reading lists")
     {:ok, content} = File.read("/Users/nash/src/aoc2024/d01/test/lists.txt")
 
     {list1, list2} =
@@ -15,16 +14,13 @@ defmodule D01Test do
       end)
       |> Enum.unzip()
 
-    IO.inspect(list1, label: "List 1")
-    IO.inspect(list2, label: "List 2")
-    IO.puts("Done reading lists")
     {list1, list2}
   end
 
-  test "greets the world" do
+  test "Day 1" do
+    IO.puts("Day 1")
     {list1, list2} = read_lists()
     zipped = Enum.zip(Enum.sort(list1), Enum.sort(list2))
-    IO.inspect(zipped, label: "Zipped")
     dist = Enum.reduce(zipped, 0, fn {a, b}, acc -> acc + abs(a - b) end)
     IO.inspect(dist, label: "Distance")
 
